0babacc62cf7f316829558c076f30bb012153820
CodeCube314 <rs3codepi314@gmail.com>
2025-02-15 20:58:52 -0800

    added limelight folder

---
+++ src/main/java/frc/robot/LimeLight/LimeLightInterface.java
@@ -0,0 +1,27 @@
+package frc.robot.LimeLight;
+
+public class LimeLightInterface {
+    private static NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+
+    public double getDoubleEntry(String entry){
+        return limelight.getEntry(entry).getDouble(0);
+    }
+    public double[] getDoubleArrayEntry(String entry){
+        return limelight.getEntry(entry).getDoubleArray(new double[0]);
+    }
+    public double getATagID(){
+        return getDoubleEntry("tid");
+    }
+    public double getTargetArea(){
+        return getDoubleEntry("ta");
+    }
+    public boolean hasValidTargets(){
+        return getDoubleEntry("tv") == 1;
+    }
+    public double getXOffset(){
+        return getDoubleEntry("tx");
+    }
+    public double[] getBotPose(){
+        return getDoubleArrayEntry("botpose_targetspace");
+    }
+}

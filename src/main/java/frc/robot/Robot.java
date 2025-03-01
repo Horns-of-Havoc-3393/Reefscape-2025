0196ec74ae64a97b3714a13e89ee8c661e615698
Colin Nelson <imposiblaa@gmail.com>
2025-03-01 12:47:42 -0800

    Ensure auto command is no longer running in teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -119,7 +119,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    chooser.get().cancel();
+  }
 
   /** This function is called periodically during operator control. */
   @Override
